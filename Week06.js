11880c281ebcafbc43b88c62e65a4a1f658ae06e
Lorenzo Turrino <lorenzo.turrino@bbc.co.uk>
2018-01-20 15:19:12 +0000

    create basic exercise,  write a function  that print the argument it is passed to it

---
+++ Week06.js
@@ -0,0 +1,40 @@
+// var students = [{
+//     name: 'Khaled',
+//     age: 39,
+//     interestes: ['Javascript', 'react']
+// }, {
+//     name: 'Kouame',
+//     age: 30,
+//     interestes: ['Javascript', 'C#']
+// }]
+
+
+
+
+// write a function called printOut that, given a string, console.log() it.
+// ex: printOut('hello world')
+// => hello world
+function printOut(arg) {
+    console.log(arg)
+}
+
+printOut('hello world');
+printOut('second statement');
+printOut('third time I call it');
+
+
+
+
+
+
+
+// write a function called printOut that, given an array, console.log() out all its elements.
+// ex: printOut([1,2,3])
+// => 1
+// => 2
+// => 3
+
+// function printOut() {
+
+// }
+
