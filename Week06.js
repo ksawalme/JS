f89fb63c8d51cacd3ca6059d8b48792388683b06
Khaled Al Sawalmeh <ksawalme@gmail.com>
2018-01-20 16:15:54 +0000

    solve some extra excerices

--- Week06.js
+++ Week06.js
@@ -1,12 +1,12 @@
-// var students = [{
-//     name: 'Khaled',
-//     age: 39,
-//     interestes: ['Javascript', 'react']
-// }, {
-//     name: 'Kouame',
-//     age: 30,
-//     interestes: ['Javascript', 'C#']
-// }]
+var students = [{
+    name: 'Khaled',
+    age: 39,
+    interestes: ['Javascript', 'react']
+}, {
+    name: 'Kouame',
+    age: 30,
+    interestes: ['Javascript', 'C#']
+}]
 
 
 
@@ -24,11 +24,21 @@ printOut('third time I call it');
 
 
 
+// write a function sum that returns the sum of 2 given numbers
+// ex: sum(3,4)
+// => 7
 
+function sum(firstNumber, secondNumber) {
+    return (firstNumber + secondNumber)
+}
 
+var firstResult = sum(2, 5);
+var secondResult = sum(5, 6);
+console.log('the first result is', firstResult);
+console.log('the secdon result is', secondResult);
+console.log('the total result is', sum(firstResult, secondResult));
 
-
-// write a function called printOut that, given an array, console.log() out all its elements.
+// write a function called printOut that, given an array, console.log() out each one of its elements
 // ex: printOut([1,2,3])
 // => 1
 // => 2
@@ -36,5 +46,21 @@ printOut('third time I call it');
 
 // function printOut() {
 
-// }
+function printOut(myArray) {
+  for ( var i=0; i<myArray.length; i++) {
+      console.log(myArray[i])
+  }
+      
+};
 
+
+
+
+
+
+
+
+
+printOut([1,2,3]);
+
+printOut(students)
